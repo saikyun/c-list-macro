041473bf1173f05edb9188832e6993ecb73f449d
Jona Ekenberg <saikyun@gmail.com>
2023-04-13 14:34:33 +0200

    removed a loop

--- list.h
+++ list.h
@@ -13,11 +13,8 @@ void *init_list(size_t elsize, int count, void *arr)
     void *l = NULL;
     l = stbds_arrgrowf_wrapper(l, elsize, 0, count);
 
-    for (int i = 0; i < count; i++)
-    {
-        memcpy(l + elsize * stbds_header(l)->length, (void *)(arr + i * elsize), elsize);
-        stbds_header(l)->length++;
-    }
+    memcpy(l + elsize * stbds_header(l)->length, (void *)arr, elsize * count);
+    stbds_header(l)->length += count;
 
     return l;
 }
